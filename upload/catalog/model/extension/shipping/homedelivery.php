e050d103d59440a4200fcd05387f023ab9930a02
condor2 <condor20_05@yahoo.it>
2019-01-06 12:13:01 +0200

    Update homedelivery.php

--- upload/catalog/model/extension/shipping/homedelivery.php
+++ upload/catalog/model/extension/shipping/homedelivery.php
@@ -1,21 +1,17 @@
 <?php
-class ModelShippingHomedelivery extends Model {
+class ModelExtensionShippingHomedelivery extends Model {
 	function getQuote($address) {
-		$this->load->language('shipping/homedelivery');
-		
-		if ($this->config->get('homedelivery_status')) {
-      		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('homedelivery_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
-		
-      		if (!$this->config->get('homedelivery_geo_zone_id')) {
-        		$status = TRUE;
-      		} elseif ($query->num_rows) {
-        		$status = TRUE;
-      		} else {
-        		$status = FALSE;
-      		}
-		} else {
-			$status = FALSE;
-		}
+		$this->load->language('extension/shipping/homedelivery');
+
+      	$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('shipping_homedelivery_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+
+   		if (!$this->config->get('shipping_homedelivery_geo_zone_id')) {
+       		$status = true;
+   		} elseif ($query->num_rows) {
+       		$status = true;
+   		} else {
+       		$status = false;
+   		}
 
 		$method_data = array();
 	
@@ -23,23 +19,22 @@ class ModelShippingHomedelivery extends Model {
 			$quote_data = array();
 			
       		$quote_data['homedelivery'] = array(
-        		'id'           => 'homedelivery.homedelivery',
+        		'code'         => 'homedelivery.homedelivery',
         		'title'        => $this->language->get('text_description'),
-        		'cost'         => $this->config->get('homedelivery_cost'),
-        		'tax_class_id' => $this->config->get('homedelivery_tax_class_id'),
-				'text'         => $this->currency->format($this->tax->calculate($this->config->get('homedelivery_cost'), $this->config->get('homedelivery_tax_class_id'), $this->config->get('config_tax')))
+        		'cost'         => $this->config->get('shipping_homedelivery_cost'),
+        		'tax_class_id' => $this->config->get('shiiping_homedelivery_tax_class_id'),
+				'text'         => $this->currency->format($this->tax->calculate($this->config->get('shipping_homedelivery_cost'), $this->config->get('shipping_homedelivery_tax_class_id'), $this->config->get('config_tax')), $this->session->data['currency'])
       		);
 
       		$method_data = array(
-        		'id'         => 'homedelivery',
+        		'code'       => 'homedelivery',
         		'title'      => $this->language->get('text_title'),
         		'quote'      => $quote_data,
-				'sort_order' => $this->config->get('homedelivery_sort_order'),
-        		'error'      => FALSE
+				'sort_order' => $this->config->get('shipping_homedelivery_sort_order'),
+        		'error'      => false
       		);
 		}
-	
+
 		return $method_data;
 	}
 }
-?>
