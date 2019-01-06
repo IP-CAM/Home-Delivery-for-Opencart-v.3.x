2914515b2fc78019e65acc26f17e221154f183ec
condor2 <condor20_05@yahoo.it>
2019-01-06 11:52:10 +0200

    Update homedelivery.php

--- upload/admin/language/en-gb/extension/shipping/homedelivery.php
+++ upload/admin/language/en-gb/extension/shipping/homedelivery.php
@@ -5,6 +5,7 @@ $_['heading_title']    = 'Home delivery';
 // Text
 $_['text_extension']   = 'Extensions';
 $_['text_success']     = 'Success: You have modified home delivery shipping!';
+$_['text_edit']        = 'Edit Home Delivery Shipping';
 
 // Entry
 $_['entry_cost']       = 'Cost:';
